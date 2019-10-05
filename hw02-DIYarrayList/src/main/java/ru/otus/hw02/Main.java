089a86b9e742b97817d289ec981677c5a8af47d0
Denis <mwarez@yahoo.com>
2019-10-05 09:43:48 +0300

    create branch hw02-DIYarrayList

---
+++ hw02-DIYarrayList/src/main/java/ru/otus/hw02/Main.java
@@ -0,0 +1,8 @@
+package ru.otus.hw02;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DIYarrayList  diYarrayList = new DIYarrayList();
+    }
+}
---
+++ hw02-DIYarrayList/src/test/java/ru/otus/hw02/TestDIYarrayList.java
@@ -0,0 +1,4 @@
+package ru.otus.hw02;
+
+public class TestDIYarrayList {
+}
