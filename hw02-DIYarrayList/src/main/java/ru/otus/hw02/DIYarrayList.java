cbb23630a92713913a6cfc91ca5de97ba917f4ab
Denis <mwarez@yahoo.com>
2019-10-07 14:48:56 +0300

    Part 1

--- hw02-DIYarrayList/src/main/java/ru/otus/hw02/DIYarrayList.java
+++ hw02-DIYarrayList/src/main/java/ru/otus/hw02/DIYarrayList.java
@@ -3,72 +3,186 @@ package ru.otus.hw02;
 
 import java.util.*;
 
+import static java.util.Objects.isNull;
+
 public class DIYarrayList<T> implements List<T> {
 
 
+    private static final int DEFAULT_CAPACITY = 30;
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    transient Object[] elementData; // non-private to simplify nested class access
+    private int size = 0;
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+    public DIYarrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: "+ initialCapacity);
+        }
+    }
+
+    public DIYarrayList() {
+        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+
+
+    public DIYarrayList(Collection<? extends T> c) {
+        elementData = c.toArray();
+        if ((size = elementData.length) != 0) {
+            if (elementData.getClass() != Object[].class)
+                elementData = Arrays.copyOf(elementData, size, Object[].class);
+        } else {
+            this.elementData = EMPTY_ELEMENTDATA;
+        }
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
+    @Override
+    public int indexOf(Object o) {
+        return indexOfRange(o, 0, size);
+    }
+
+    int indexOfRange(Object o, int start, int end) {
+        Object[] es = elementData;
+        if (o == null) {
+            for (int i = start; i < end; i++) {
+                if (es[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = start; i < end; i++) {
+                if (o.equals(es[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new MyIterator();
+    }
+
+    public class MyIterator<T> implements Iterator<T> {
+        private int index;
+        MyIterator() { index = 0; }
+
+        @Override
+        public boolean hasNext() { return index < size(); }
+
+        @Override
+        public T next() { return (T) get(index++); }
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return Arrays.copyOf(elementData, size);
     }
 
     @Override
-    public <T1> T1[] toArray(T1[] a) {
-        return null;
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size)
+            return (T[]) Arrays.copyOf(elementData, size, a.getClass());
+        System.arraycopy(elementData, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+    private int newCapacity(int minCapacity) {
+        // overflow-conscious code
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity <= 0) {
+            if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA)
+                return Math.max(DEFAULT_CAPACITY, minCapacity);
+            if (minCapacity < 0) // overflow
+                throw new OutOfMemoryError();
+            return minCapacity;
+        }
+        return (newCapacity - MAX_ARRAY_SIZE <= 0)
+                ? newCapacity
+                : hugeCapacity(minCapacity);
+    }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE)
+                ? Integer.MAX_VALUE
+                : MAX_ARRAY_SIZE;
+    }
+
+    private Object[] grow(int minCapacity) {
+        return elementData = Arrays.copyOf(elementData,
+                newCapacity(minCapacity));
+    }
+
+    private Object[] grow() {
+        return grow(size + 1);
+    }
+
+    private void add(T e, Object[] elementData, int s) {
+        if (s == elementData.length)
+            elementData = grow();
+        elementData[s] = e;
+        size = s + 1;
     }
 
     @Override
     public boolean add(T t) {
-        return false;
+        add(t, elementData, size);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        throw new UnsupportedOperationException("remove(Object o)");
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("containsAll(Collection<?> c)");
     }
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException("addAll(Collection<? extends T> c)");
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException("addAll(int index, Collection<? extends T> c)");
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("removeAll(Collection<?> c)");
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("retainAll(Collection<?> c)");
     }
 
     @Override
@@ -76,9 +190,14 @@ public class DIYarrayList<T> implements List<T> {
 
     }
 
+    T elementData(int index) {
+        return (T) elementData[index];
+    }
+
     @Override
     public T get(int index) {
-        return null;
+        Objects.checkIndex(index, size);
+        return elementData(index);
     }
 
     @Override
@@ -96,28 +215,42 @@ public class DIYarrayList<T> implements List<T> {
         return null;
     }
 
-    @Override
-    public int indexOf(Object o) {
-        return 0;
+
+    public int lastIndexOf(Object o) {
+        return lastIndexOfRange(o, 0, size);
     }
 
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
+    int lastIndexOfRange(Object o, int start, int end) {
+        Object[] es = elementData;
+        if (o == null) {
+            for (int i = end - 1; i >= start; i--) {
+                if (es[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = end - 1; i >= start; i--) {
+                if (o.equals(es[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
     public ListIterator<T> listIterator() {
-        return null;
+        throw new UnsupportedOperationException(" listIterator()");
     }
 
     @Override
     public ListIterator<T> listIterator(int index) {
-        return null;
+        throw new UnsupportedOperationException("listIterator(int index)");
     }
+
 
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
-        return null;
+        throw new UnsupportedOperationException("subList(int fromIndex, int toIndex)");
     }
 }
--- hw02-DIYarrayList/src/main/java/ru/otus/hw02/Main.java
+++ hw02-DIYarrayList/src/main/java/ru/otus/hw02/Main.java
@@ -1,8 +1,42 @@
 package ru.otus.hw02;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        DIYarrayList  diYarrayList = new DIYarrayList();
+
+        Integer [] integers = new Integer[30];
+
+
+        System.out.println("Array integers");
+        for(int i=0; i<30; i++) {
+            int i1 = new Random().nextInt(100);
+            integers[i] = i1;
+            System.out.print(i1);
+        }
+        System.out.println("");
+
+        //1. Collections.addAll(Collection<? super T> c, T... elements)
+        DIYarrayList<Integer>  diYarrayList = new DIYarrayList(30);
+        Collections.addAll(diYarrayList,integers);
+        System.out.println("View diYarrayList");
+        diYarrayList.forEach(System.out::print);
+        System.out.println("");
+
+        //2. Collections.static <T> void copy(List<? super T> dest, List<? extends T> src)
+        List<Integer> diYarrayListNew = new ArrayList<>(30);
+        for(int i=0; i<30; i++) {
+            diYarrayListNew.add(i, null);
+        }
+        Collections.copy(diYarrayListNew,diYarrayList);
+        System.out.println("View diYarrayListNew");
+        diYarrayListNew.forEach(System.out::print);
+
+//        Collections.sort();
     }
 }
