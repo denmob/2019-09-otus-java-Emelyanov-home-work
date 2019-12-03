5971c0e7be9043f3b5e3081eec345b7fc670b30f
D.Yemelianov <mwarez@yahoo.com>
2019-12-03 09:59:03 +0200

    add example gson

--- hw08-json/src/main/java/ru/otus/hw08/Main.java
+++ hw08-json/src/main/java/ru/otus/hw08/Main.java
@@ -1,10 +1,24 @@
 package ru.otus.hw08;
 
+
+import com.google.gson.Gson;
+
 public class Main {
 
     public static void main(String[] args)  {
+
         System.out.println("hw08-json");
 
+        Gson gson = new Gson();
+        BagOfPrimitives obj = new BagOfPrimitives(22, "test", 10);
+        System.out.println(obj);
+
+        String json = gson.toJson(obj);
+        System.out.println(json);
+
+        BagOfPrimitives obj2 = gson.fromJson(json, BagOfPrimitives.class);
+        System.out.println(obj.equals(obj2));
+        System.out.println(obj2);
 
     }
 }
